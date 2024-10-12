b06775881e23b7b1d8fb5f1e37ea973da5086251
Manujaya Sasanka <manujaya77sasanka@gmail.com>
2024-10-12 18:38:04 +0530

    feat: Add Header component and footer to layout.js
    - Imported Header component into layout.js and added it to the layout structure
    - Added a footer section with copyright information for 2024
    - Ensured consistent styling with borders, padding, and text alignment

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,7 @@
 
 import { Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "./components/layout/Header";
 
 const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
@@ -44,7 +45,11 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main main className="max-w-4xl mx-auto p-4">
+          <Header />
           {children}
+          <footer className="border-t p-8 text-center text-gray-500 mt-8">
+            &copy; 2024 All rights reserved 
+          </footer>
         </main>
       </body>
     </html>
